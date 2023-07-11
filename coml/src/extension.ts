29b9b13898887db9b86d4059d8dc0f004c73e6bd
Yuge Zhang <scottyugochang@gmail.com>
2023-07-11 17:43:35 +0800

    boilerplate

--- coml/src/extension.ts
+++ coml/src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-import { CoMLPanel } from "./panels/CoMLPanel";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,13 +9,10 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "coml" is now active!');
 	
-	// Create the show hello world command
-	const showHelloWorldCommand = vscode.commands.registerCommand("coml.showHelloWorld", () => {
-		CoMLPanel.render(context.extensionUri);
-	});
-	
-	// Add command to the extension context
-	context.subscriptions.push(showHelloWorldCommand);
+	const provider = new CoMLPanelProvider(context.extensionUri);
+
+	context.subscriptions.push(
+		vscode.window.registerWebviewViewProvider(CoMLPanelProvider.viewType, provider));
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
@@ -32,3 +28,107 @@ export function activate(context: vscode.ExtensionContext) {
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
+
+class CoMLPanelProvider implements vscode.WebviewViewProvider {
+
+	public static readonly viewType = 'coml.comlView';
+
+	private _view?: vscode.WebviewView;
+
+	constructor(
+		private readonly _extensionUri: vscode.Uri,
+	) { }
+
+	public resolveWebviewView(
+		webviewView: vscode.WebviewView,
+		context: vscode.WebviewViewResolveContext,
+		_token: vscode.CancellationToken,
+	) {
+		this._view = webviewView;
+
+		webviewView.webview.options = {
+			// Allow scripts in the webview
+			enableScripts: true,
+
+			localResourceRoots: [
+				vscode.Uri.joinPath(this._extensionUri, "out"),
+        vscode.Uri.joinPath(this._extensionUri, "app/build")
+			]
+		};
+
+		webviewView.webview.html = this._getHtmlForWebview(webviewView.webview);
+
+		this._setWebviewMessageListener(webviewView.webview);
+	}
+
+	private _getHtmlForWebview(webview: vscode.Webview) {
+    // The CSS file from the React build output
+    const stylesUri = getUri(webview, this._extensionUri, [
+      "app",
+      "build",
+      "static",
+      "css",
+      "main.css",
+    ]);
+    // The JS file from the React build output
+    const scriptUri = getUri(webview, this._extensionUri, [
+      "app",
+      "build",
+      "static",
+      "js",
+      "main.js",
+    ]);
+    const nonce = getNonce();
+
+    // Tip: Install the es6-string-html VS Code extension to enable code highlighting below
+    return /*html*/ `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="utf-8">
+          <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no">
+          <meta name="theme-color" content="#000000">
+          <meta http-equiv="Content-Security-Policy" content="default-src 'none'; style-src ${webview.cspSource}; script-src 'nonce-${nonce}';">
+          <link rel="stylesheet" type="text/css" href="${stylesUri}">
+          <title>Hello World</title>
+        </head>
+        <body>
+          <noscript>You need to enable JavaScript to run this app.</noscript>
+          <div id="root"></div>
+          <script nonce="${nonce}" src="${scriptUri}"></script>
+        </body>
+      </html>
+    `;
+	}
+
+  private _setWebviewMessageListener(webview: vscode.Webview) {
+    webview.onDidReceiveMessage(
+      (message: any) => {
+        const command = message.command;
+        const text = message.text;
+
+        switch (command) {
+          case "hello":
+            // Code that should run in response to the hello message command
+            vscode.window.showInformationMessage(text);
+            return;
+          // Add more switch case statements here as more webview message commands
+          // are created within the webview context (i.e. inside media/main.js)
+        }
+      },
+    );
+  }
+}
+
+function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
+
+function getUri(webview: vscode.Webview, extensionUri: vscode.Uri, pathList: string[]) {
+  return webview.asWebviewUri(vscode.Uri.joinPath(extensionUri, ...pathList));
+}
