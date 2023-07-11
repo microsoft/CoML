87f26a0b9b6f2ff75e39c52b94481a4f731ca4c4
Yuge Zhang <scottyugochang@gmail.com>
2023-07-11 17:28:05 +0800

    working in main panel

--- coml/src/extension.ts
+++ coml/src/extension.ts
@@ -1,14 +1,22 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import { CoMLPanel } from "./panels/CoMLPanel";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-
+export function activate(context: vscode.ExtensionContext) {	
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "coml" is now active!');
+	
+	// Create the show hello world command
+	const showHelloWorldCommand = vscode.commands.registerCommand("coml.showHelloWorld", () => {
+		CoMLPanel.render(context.extensionUri);
+	});
+	
+	// Add command to the extension context
+	context.subscriptions.push(showHelloWorldCommand);
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
