a60d5e1d5d3f8b77bbe2791a471a1d0b1d7d4416
Yuge Zhang <scottyugochang@gmail.com>
2023-07-13 00:02:18 +0800

    app.tsx

--- coml/app/src/App.tsx
+++ coml/app/src/App.tsx
@@ -1,6 +1,12 @@
 import { vscode } from "./utilities/vscode";
-import { VSCodeButton } from "@vscode/webview-ui-toolkit/react";
+import { VSCodeButton, VSCodeTextField, VSCodeTextArea, VSCodeDivider } from "@vscode/webview-ui-toolkit/react";
 import "./App.css";
+import "@vscode/codicons/dist/codicon.css";
+import { useEffect, useRef, useState } from "react";
+import autosize from "autosize";
+
+import { chatWithGPT } from "./chat";
+import { HumanMessage, SystemMessage, BaseMessage } from "langchain/schema";
 
 function App() {
   function handleHowdyClick() {
@@ -10,10 +16,49 @@ function App() {
     });
   }
 
+  const [messages, setMessages] = useState<BaseMessage[]>([]);
+
+  function submit() {
+    const textInput = (textareaRef.current as any).value;
+    console.log(textInput);
+    (textareaRef.current as any).value = "";  // clear input
+    setMessages([...messages, new HumanMessage(textInput)]);
+  }
+
+  useEffect(() => {
+    autosize((textareaRef.current as any).shadowRoot.querySelectorAll("textarea"));
+  });
+
+  useEffect(() => {
+    if (messages.length > 0 && (messages[messages.length - 1] instanceof HumanMessage)) {
+      chatWithGPT(messages).then((response) => {
+        setMessages([...messages, response]);
+      });
+    }
+  }, [messages]);
+
+  const textareaRef = useRef(null);
+
+  const messageDisplay: JSX.Element[] = [];
+  for (let i = 0; i < messages.length; ++i) {
+    if (messages[i] instanceof HumanMessage) {
+      messageDisplay.push(<h3 key={`message-${i}-role`}>User</h3>);
+    } else {
+      messageDisplay.push(<h3 key={`message-${i}-role`}>ChatML</h3>);
+    }
+    messageDisplay.push(<p key={`message-${i}-content`}>{messages[i].content}</p>);
+    if (i < messages.length - 1) {
+      messageDisplay.push(<VSCodeDivider key={`message-${i}-divider`} />);
+    }
+  }
   return (
     <main>
-      <h1>Hello World!</h1>
-      <VSCodeButton onClick={handleHowdyClick}>Howdy!</VSCodeButton>
+      <h1>ChatML</h1>
+      {messageDisplay}
+      <div className="chat-box">
+        <span className="send codicon codicon-send" onClick={submit}></span>
+        <VSCodeTextArea resize="none" className="text" name="text" placeholder="Ask anything" ref={textareaRef} rows={1} />
+      </div>
     </main>
   );
 }
